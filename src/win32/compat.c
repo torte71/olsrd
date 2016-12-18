e1c068e67156ff79d52a03d7e9d9b21a8799213e
torte71 <info@netztorte.de>
2016-12-18 19:28:35 +0100

    Win32: Don't depend on libwinpthread for clock_gettime()

--- src/win32/compat.c
+++ src/win32/compat.c
@@ -72,6 +72,7 @@
 
 #include "defs.h"
 
+void getFILETIMEoffset(LARGE_INTEGER *t);
 void PError(char *Str);
 void WinSockPError(char *Str);
 
@@ -144,6 +145,70 @@ inet_aton(const char *AddrStr, struct in_addr *Addr)
   return 1;
 }
 #endif /* !defined(MINGW_VERSION) || MINGW_VERSION < 40600 */
+
+/* clock_gettime() is not available on Win32 (unless we link libwinpthread)
+ * this clock_gettime() and dependency getFILETIMEoffset() is based on
+ * http://stackoverflow.com/questions/5404277/porting-clock-gettime-to-windows
+ * thanks to http://stackoverflow.com/users/658776/carl-staelin
+ */
+void
+getFILETIMEoffset(LARGE_INTEGER *t)
+{
+    SYSTEMTIME s;
+    FILETIME f;
+
+    s.wYear = 1970;
+    s.wMonth = 1;
+    s.wDay = 1;
+    s.wHour = 0;
+    s.wMinute = 0;
+    s.wSecond = 0;
+    s.wMilliseconds = 0;
+    SystemTimeToFileTime(&s, &f);
+    t->QuadPart = f.dwHighDateTime;
+    t->QuadPart <<= 32;
+    t->QuadPart |= f.dwLowDateTime;
+}
+
+int
+clock_gettime(int dummy, struct timespec *ts)
+{
+    LARGE_INTEGER           t;
+    FILETIME                f;
+    double                  microseconds;
+    static LARGE_INTEGER    offset;
+    static double           frequencyToMicroseconds;
+    static int              initialized = 0;
+    static BOOL             usePerformanceCounter = 0;
+
+    (void)dummy; /* suppress "unused" warning */
+    if (!initialized) {
+        LARGE_INTEGER performanceFrequency;
+        initialized = 1;
+        usePerformanceCounter = QueryPerformanceFrequency(&performanceFrequency);
+        if (usePerformanceCounter) {
+            QueryPerformanceCounter(&offset);
+            frequencyToMicroseconds = (double)performanceFrequency.QuadPart / 1000000.;
+        } else {
+            getFILETIMEoffset(&offset);
+            frequencyToMicroseconds = 10.;
+        }
+    }
+    if (usePerformanceCounter) QueryPerformanceCounter(&t);
+    else {
+        GetSystemTimeAsFileTime(&f);
+        t.QuadPart = f.dwHighDateTime;
+        t.QuadPart <<= 32;
+        t.QuadPart |= f.dwLowDateTime;
+    }
+
+    t.QuadPart -= offset.QuadPart;
+    microseconds = (double)t.QuadPart / frequencyToMicroseconds;
+    t.QuadPart = microseconds;
+    ts->tv_sec = t.QuadPart / 1000000;
+    ts->tv_nsec = (t.QuadPart % 1000000)*1000;
+    return (0);
+}
 
 char *
 StrError(unsigned int ErrNo)
